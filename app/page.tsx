9369f72eac2679ef7a25056eae37084477791bb1
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-27 20:53:20 -0500

    build HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,59 @@
 import React from "react";
+import {
+  BoltIcon,
+  ExclamationTriangleIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>AI-Chat</h1>
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="text-5xl font-bold mb-20">AI-Chat</h1>
+      <div className="flex space-x-5 text-center">
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="h-8 w-8" />
+            <h2>Examples</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              "Explain quantum computing in simple terms"
+            </p>
+            <p className="infoText">
+              "Recommend gifts for a 10 year old's birthday"
+            </p>
+            <p className="infoText">"Which country won World Cup 2022?"</p>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <BoltIcon className="h-8 w-8" />
+            <h2>Capabilities</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Provides concise and clear guidance</p>
+            <p className="infoText">Saves users' precious time</p>
+            <p className="infoText">
+              Offers seamless ad-free search experience
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <ExclamationTriangleIcon className="h-8 w-8" />
+            <h2>Limitations</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              May occasionally generate incorrect information
+            </p>
+            <p className="infoText">May occasionally produce biased content</p>
+            <p className="infoText">
+              Limited knowledge of world and events after 2021
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
